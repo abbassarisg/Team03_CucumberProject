5b82f08e578a9cbed9689751b1e9e4aa7b39aa62
Asım Ates <asmates89@gmail.com>
2022-11-22 13:00:08 +0300

    guncel

--- src/test/java/pages/AsimPage.java
+++ src/test/java/pages/AsimPage.java
@@ -1,4 +1,37 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class AsimPage {
+
+    public AsimPage() {
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy (xpath = "//a[@href='#appointment'][1]")
+    public WebElement makeAnAppointment;
+
+    @FindBy (xpath = "//input[@id='firstName']")
+    public WebElement firstNameEmpty;
+
+    @FindBy (xpath = "//input[@value='adem']")
+    public WebElement firstNameFull;
+
+    @FindBy (xpath = "//div[text()='Your FirstName is required.']")
+    public WebElement firstNameWarning;
+
+    @FindBy (xpath = "//input[@id='lastName']")
+    public WebElement lastNameEmpty;
+
+    @FindBy (xpath = "//input[@value='Bakırcı']")
+    public WebElement lastNameFull;
+
+    @FindBy (xpath = "//div[text()='Your LastName is required.']")
+    public WebElement lastNameWarning;
+
+
 }
