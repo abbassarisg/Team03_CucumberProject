6237a702be5eb778fc3f379b355507d89694b0d2
afra94 <kolotafra@gmail.com>
2022-11-25 13:13:45 +0300

    25.11.2022

---
+++ src/test/java/api/pojos/AfraPojo/AppointmentCountryPysician.java
@@ -0,0 +1,45 @@
+package api.pojos.AfraPojo;
+
+import io.cucumber.messages.internal.com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AppointmentCountryPysician {
+    public int id;
+    public String name;
+
+    public AppointmentCountryPysician() {
+    }
+
+    public AppointmentCountryPysician(int id, String name) {
+        this.id = id;
+        this.name = name;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "AppointmentCountryPysician{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/test/resources/features/apiFeatures/Us_010_Api.feature
@@ -0,0 +1,8 @@
+Feature: Verify appointment
+  @Api
+  Scenario: Us_010_Tc_005 Verify appointment with api
+    Given Doctor lokman set the base url
+    When  Doctor send the request and get the response
+    Then  Doctor status code should be 200
+    Then  Doctor verify response
+
